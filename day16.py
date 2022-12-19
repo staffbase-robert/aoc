4ebd641ff30309c8a610d68b40aa88dd96350732
Robert Hecht <robert.hecht@staffbase.com>
2022-12-20 00:50:36 +0100

    day 16 part 1

--- day16.py
+++ day16.py
@@ -20,36 +20,17 @@ def find_by_name(valves, name):
             return valve
     return None
 class Valve():
-    def __init__(self, name, flowrate, doors, reward=0) -> None:
+    def __init__(self, name, flowrate, doors, ) -> None:
         self.name = name
         self.flowrate = flowrate
         self.doors = doors
         self.connections = None
-        self.reward = reward
 
     def connect(self, all_valves):
         self.connections = [(find_by_name(all_valves, door), 1) for door in self.doors]
-        if self.flowrate != 0:
-            new_valve = Valve(f"{self.name}-OV", 0, [self.name], self.flowrate)
-            self.connections.append((new_valve, 1))
-            all_valves.append(new_valve)
 
     def __repr__(self) -> str:
-        return f"{self.name}: flowrate: {self.flowrate}, doors: {self.doors}, reward: {self.reward}"
-
-
-def simplify(valve, visited=set()):
-    visited.add(valve)
-    for ci in range(len(valve.connections)):
-        edge = valve.connections[ci]
-        con, cost = edge
-        if con.flowrate == 0:
-            inherited = list(filter(lambda edge: edge[0].name != valve.name, child.connections))
-            inherited = [(inh[0], inh[1] + cost) for inh in inherited]
-            valve.chonections = valve.chonections[0:ci] + valve.chonections[ci+1:] + inherited
-    for con, _ in valve.connections:
-        if con not in visited:
-            simplify(con, visited=visited)
+        return f"{self.name}: flowrate: {self.flowrate}, doors: {self.doors}"
 
 import graphviz
 
@@ -57,7 +38,7 @@ def visualize(valve):
     e = graphviz.Graph('ER', filename='er.gv', engine='neato')
     def draw_edges(valve, visited = set()):
         visited.add(valve)
-        e.node(valve.name, label=f"{valve.name} - {valve.reward}")
+        e.node(valve.name, label=f"{valve.name} - {valve.flowrate}")
         for con, cost in valve.connections:
             e.edge(valve.name, con.name, label=f"{cost}", len="3")
 
@@ -69,23 +50,25 @@ def visualize(valve):
     return e
 
 with open("input-16") as f:
-    # IN = f.read()
-    IN = example
+    IN = f.read()
+    # IN = example
     valves = [Valve(match[0], int(match[1]), match[2].split(', ')) for match in re.findall(r"Valve (\w\w) has flow rate=(\d+); tunnels? leads? to valves? (.*)", IN, re.MULTILINE)]
     for valve in valves:
         valve.connect(valves)
 
-
-    # simplify(valves[0])
     # visualize(valves[0]).view()
-
-    paths = []
-    
-    valves_with_reward = list(filter(lambda v: v.reward > 0, valves))
-    for v in valves_with_reward:
+    rewards = list(filter(lambda v: v.flowrate > 0, valves))
+    rewards = sorted(rewards, key=lambda v: v.flowrate, reverse=True)
+    for v in rewards:
         print(v)
 
+
+    cache = {}
     def find_routes(v, target):
+        key = (v, target)
+        if key in cache:
+            return cache[key]
+
         def traceback(v, target, routes, history=[]):
             history.append(v)
             for con, _ in v.connections:
@@ -97,31 +80,36 @@ with open("input-16") as f:
 
         routes = []
         traceback(v, target, routes)
+        cache[key] = routes
         return routes
 
     max_clock = 30
-
     best = -1
-    def nav(current, targets, minute=0, rewards=[], paths=[]):
+    def nav(current, targets, minute=0, rewards=[], order_of_opens=[]):
         global best
+
+        if len(targets) == 0 or minute <= max_clock:
+            score = sum([r[0] * (max_clock - r[1]) for r in rewards])
+            if score > best:
+                print(f"found new best {score},\nrewards={', '.join([str(r) for r in rewards])}")
+                best = score
+
         for vi in range(len(targets)):
             v = targets[vi]
             routes = find_routes(current, v)
             shortest = min(routes, key=lambda r: len(r))
             remaining = targets[0:vi] + targets[vi+1:]
-            clock = minute + len(shortest)
-            new_paths = copy.copy(paths)
-            new_paths.append(shortest)
-            if len(remaining) > 0:
+            clock = minute + len(shortest) + 1
+
+            new_order_of_opens = copy.copy(order_of_opens)
+            new_order_of_opens.append((v.name, v.flowrate))
+            new_rewards = copy.copy(rewards)
+            new_rewards.append([v.flowrate, clock, v.name])
+            if len(remaining) >= 0:
                 if clock <= max_clock:
-                    new_rewards = copy.copy(rewards)
-                    new_rewards.append((max_clock-clock) * v.reward)
-                    nav(v, remaining, clock, rewards=new_rewards, paths=new_paths)
-            else:
-                score = sum(rewards)
-                if score > best:
-                    print(f"found new best", [valve.name for path in paths for valve in path])
-                    best = score
-    start = valves[0]
-    nav(start ,copy.copy(valves_with_reward))
+                    # print(f"--- reached new reward in minute={clock} - collecting {reward} reward ---")
+                    nav(v, remaining, clock, rewards=new_rewards, order_of_opens=new_order_of_opens)
+        
+    start = find_by_name(valves, "AA")
+    nav(start ,copy.copy(rewards))
     print(best)
